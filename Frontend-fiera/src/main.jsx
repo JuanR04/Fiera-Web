3cbdeccf96feebede51bc8957129815bf28dd73b
JuanR04 <juancrg2004@hotmail.com>
2025-07-02 12:17:17 -0500

    layout implement

--- Frontend-fiera/src/main.jsx
+++ Frontend-fiera/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/home';
+import Layout from './layout/layout';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
---
+++ Frontend-fiera/src/pages/home/home.jsx
@@ -0,0 +1,7 @@
+const Home = () =>{
+    return(
+        <h1>hola soy home</h1>
+    )
+}
+
+export default Home;
