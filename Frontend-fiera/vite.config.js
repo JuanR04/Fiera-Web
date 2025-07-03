627234e0ba2fae562104b5eb2be07860fb62c196
JuanR04 <juancrg2004@hotmail.com>
2025-07-03 09:44:06 -0500

    configuracion de vite.config

--- Frontend-fiera/vite.config.js
+++ Frontend-fiera/vite.config.js
@@ -1,10 +1,15 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+// Carga las variables de entorno
+dotenv.config()
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Permite acceso externo
-    allowedHosts: ['ghhmdv-ip-186-86-52-214.tunnelmole.net'] // Cambia esto si el dominio cambia
+    host: true,
+    allowedHosts: [process.env.VITE_HOST_TUNNEL]
   }
 })
