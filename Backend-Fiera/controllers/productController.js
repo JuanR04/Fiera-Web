8f1656b91c553be5e743adfb7d10e7fb74bd0d4a
JuanR04 <juancrg2004@hotmail.com>
2025-07-11 11:33:51 -0500

    correcion de bugs

--- Backend-Fiera/controllers/productController.js
+++ Backend-Fiera/controllers/productController.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-import { findProducts, CreateProducts, UpdateProductsId } from "../models/productModel.js";
+
+import { findProducts, CreateProducts, UpdateProductsId,findProductById, deleteProducts } from "../models/productModel.js";
 import { createDetailsProduct, UpdateDetailsProduct } from "../models/productDetailModel.js";
-=======
-import {findProducts, CreateProducts, findProductById, deleteProducts} from "../models/productModel.js";
-import { createDetailsProduct } from "../models/productDetailModel.js";
->>>>>>> 21b5bf3d30b5d8bf2fa13b947bdca7feb6eabd28
 import cloudinary from "../config/cloudinaryConfig.js";
 import multer from 'multer';
 import pool from "../config/db.js";
@@ -24,7 +20,6 @@ export const getProducts = async (req, res) => {
 export const registerProduct = [
     upload.single('image'),
     async (req, res) => {
-        console.log(req.body);
         const { num_referencia, category, description, name, subcategory, type, size_min, size_max, material, colors } = req.body;
 
         if (
@@ -53,7 +48,7 @@ export const registerProduct = [
         }
     }
 ]
-<<<<<<< HEAD
+
 
 export const UpdateProduct = [upload.single('image'),
 async (req, res) => {
@@ -72,8 +67,13 @@ async (req, res) => {
         return res.status(400).json({ message: "Todos los campos son obligatorios excepto la descripción." });
     }
     try {
-        const { rows } = await pool.query('SELECT url_image FROM producto WHERE id_producto=$1', [id_producto]);
-        const oldimageurl = rows[0]?.url_image;
+        const product = await findProductById(id_producto);
+
+        if (!product) {
+            return res.status(404).json({ message: "Producto no encontrado" });
+        }
+        
+        const oldimageurl = product.url_image;
 
         const result = await cloudinary.uploader.upload(req.file.path);
         const url_image = result.secure_url;
@@ -94,50 +94,43 @@ async (req, res) => {
 
 
 }]
-=======
 // Controlador para obtener un producto por ID
 export const getProductById = async(req, res) => {
     try {
-        const { id } = req.params;// obtener el id del producto desde los parametros de la URL.
-        const product = await findProductById(id);// buscar el producto por su ID en la base de datos.
+        const { id } = req.params;
+        const product = await findProductById(id);
         
-        if (!product) { // Si el producto no existe, se retorna un error  
+        if (!product) {
             return res.status(404).json({ message: "Producto no encontrado" });
         }
         
         res.status(200).json(product);
     } catch (error) {
-        console.error("Error al obtener producto:", error);
-        res.status(500).json({
-            message: "Error en el servidor al obtener el producto",
-            error: error.message
-        });
+        res.status(500).json({message: "Error en el servidor al obtener el producto",error: error.message});
     }
 };
-// Controlador para la eliminacion  de productos:
+
 export const deleteProductById = async(req,res)=>{
     try{
-        const {id} = req.params; // Aqui se obtiene el id de los parametros de la URL, la cual es enviada desde el cliente.
+        const {id} = req.params; 
 
-        // verficar si el producto existe:
+        
         const product = await findProductById(id);
         if(!product){
             return res.status(404).json({message: "No se encontró el producto"});
         }
-        // Si hay una imagen en cloudinary eliminarla:
+        
         if(product.url_image){
-            // Se extrae el public_id de la URL de la imagen para eliminarla de Cloudinary.
+            
             const publicId = product.url_image.split('/').pop().split('.')[0];
             await cloudinary.uploader.destroy(publicId);
         }
-        // Eliminar el producto:
+        
         const deleteProduct = await deleteProducts(id);
 
-        // status 200 indica que la solicitud se ha procesado correctamente.
         res.status(200).json({message: "Producto eliminado correctamente", product: deleteProduct});// Se envia el producto eliminado como respuesta.
 
     }catch(error){
         res.status(500).json({message: "Error en el servidor", error: error.message});
     }
 }
->>>>>>> 21b5bf3d30b5d8bf2fa13b947bdca7feb6eabd28
